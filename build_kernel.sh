f974f4902bb02baee50f14bb98d408ad0dbe9bb5
Captain Throwback <captainthrowback@hotmail.com>
2021-08-21 16:47:20 -0400

    build script cleanup
    fix build warnings

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,6 +6,7 @@ Android_Build=~/android/build/AOSP/android-11
 Clang_Google=prebuilts/clang/host
 Prebuilt_Clang=clang-r383902
 GCC_Google_Arm64=prebuilts/gcc/linux-x86/aarch64
+GCC_Google_Arm32=prebuilts/gcc/linux-x86/arm
 Kernel_Output_Path=out/arch/arm64/boot
 
 echo
@@ -32,19 +33,47 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=$Android_Build/$Clang_Google/linux-x86/$Prebuilt_Clang/bin
-export PATH=${CLANG_PATH}:${PATH}
+export CLANG_PREBUILT_BIN=$Android_Build/$Clang_Google/linux-x86/$Prebuilt_Clang/bin
+export PATH=${CLANG_PREBUILT_BIN}:${PATH}
 export DTC_EXT=$Kernel_Root/dtc-aosp
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$Android_Build/$GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$Android_Build/$GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+export CROSS_COMPILE_COMPAT=$Android_Build/$GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN=$Android_Build/$GCC_Google_Arm64/aarch64-linux-android-4.9/bin
+export LINUX_GCC_CROSS_COMPILE_ARM32_PREBUILTS_BIN=$Android_Build/$GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/
+
+export FILES="
+arch/arm64/boot/Image.gz
+arch/arm64/boot/Image
+vmlinux
+System.map
+"
+
+export CLANG_AR=$CLANG_PREBUILT_BIN/llvm-ar
+export CLANG_CC=$CLANG_PREBUILT_BIN/clang
+export CLANG_CCXX=$CLANG_PREBUILT_BIN/clang++
+export CLANG_LD=$CLANG_PREBUILT_BIN/ld.lld
+export CLANG_LDLTO=$CLANG_PREBUILT_BIN/ld.lld
+export CLANG_NM=$CLANG_PREBUILT_BIN/llvm-nm
+export CLANG_STRIP=$CLANG_PREBUILT_BIN/llvm-strip
+export CLANG_OC=$CLANG_PREBUILT_BIN/llvm-objcopy
+export CLANG_OD=$CLANG_PREBUILT_BIN/llvm-objdump
+export CLANG_OS=$CLANG_PREBUILT_BIN/llvm-size
+export CLANG_RE=$CLANG_PREBUILT_BIN/llvm-readelf
+
+export CC=$CLANG_CC
+export HOST_CC=$CLANG_CC
+export LD=$CLANG_LD
+
+export ASUS_BUILD_PROJECT=SAKE
 
 echo
 echo "Choose DEFCONFIG"
 echo 
 
-DEFCONFIG=($(find $Defconfig_Folder -type f -exec echo '{}' \; | awk -F'configs/' '{print $NF}'))
+DEFCONFIG=($(find $Defconfig_Folder -iname '*config*' -type f -exec echo '{}' \; | awk -F'configs/' '{print $NF}'))
 select choice in "${DEFCONFIG[@]}"; do
-    make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out "$choice"
+    make LLVM=1 CC=$CLANG_CC LD=$CLANG_LD AR=$CLANG_AR STRIP=$CLANG_STRIP OBJCOPY=$CLANG_OC NM=$CLANG_NM OBJDUMP=$CLANG_OD OBJSIZE=$CLANG_OS READELF=$CLANG_RE HOSTCC=$CLANG_CC HOSTCXX=$CLANG_CCXX HOSTAR=$CLANG_AR HOSTLD=$CLANG_LD O=out "$choice"
 	break
 done
 
@@ -52,7 +81,7 @@ echo
 echo "Build The Good Stuff"
 echo 
 
-time make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j$(($(nproc) * 2))
+time make LLVM=1 CC=$CLANG_CC LD=$CLANG_LD AR=$CLANG_AR STRIP=$CLANG_STRIP OBJCOPY=$CLANG_OC NM=$CLANG_NM OBJDUMP=$CLANG_OD OBJSIZE=$CLANG_OS READELF=$CLANG_RE HOSTCC=$CLANG_CC HOSTCXX=$CLANG_CCXX HOSTAR=$CLANG_AR HOSTLD=$CLANG_LD O=out -j$(($(nproc) * 2))
 
 if [ -e $Kernel_Output_Path/Image ]; then
 	echo
